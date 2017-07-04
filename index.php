dbde6a58f043ac2800187ead84301a6783d03840
Антон <>
2017-07-04 22:38:06 +0200

    stoped at urls for images

--- index.php
+++ index.php
@@ -42,10 +42,12 @@
     function myFunction(xml) {
       var i;
       var xmlDoc = xml.responseXML;
-      var table="<tr><th>Category</th><th>Title</th><th>Link</th><th>Date</th></tr>";
+      var table="<tr><th></th><th>Category</th><th>Title</th><th>Link</th><th>Date</th></tr>";
       var x = xmlDoc.getElementsByTagName("item");
       for (i = 0; i <x.length; i++) {
         table += "<tr><td>" +
+        x[i].getElementsByTagName(".//img[1]/@src")[0].childNodes[0].nodeValue +
+        "</td><td>" +
         x[i].getElementsByTagName("category")[0].childNodes[0].nodeValue +
         "</td><td>" +
         x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +
