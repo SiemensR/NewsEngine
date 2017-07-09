88bbac0c39924b6ae0a7f172e1fae298ae6bbb29
Антон <>
2017-07-09 21:48:23 +0200

    xml doesnt work anymore

--- index.php
+++ index.php
@@ -26,7 +26,41 @@
     <button type="button" onclick="location.reload();">Reload page</button>
     <br><br>
     <table id="demo"></table>
+<script>
+    function loadDoc() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          myFunction(this);
+        }
+      };
+      xhttp.open("GET", "news.xml", true);
+      xhttp.send();
+    }
 
+    function myFunction(xml) { //here is a function for adding all news from xml
+      var i;
+      var xmlDoc = xml.responseXML;
+      var table="<tr><th></th><th>Category</th><th>Title</th><th>Link</th><th>Date</th></tr>";
+      var x = xmlDoc.getElementsByTagName("item");
+      for (i = 0; i <x.length; i++) {
+        table += "<tr><td>" +
+        "<img src=" + x[i].getElementsByTagName("enclosure")[0].getAttribute["url"] + ">" +
+        "</td><td>" +
+        x[i].getElementsByTagName("category")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue +
+        "</td><td>"+
+        "<a href=" + x[i].getElementsByTagName("guid")[0].childNodes[0].nodeValue + " target=\"_blank\">" + x[i].getElementsByTagName("guid")[0].childNodes[0].nodeValue + "</a>" +
+        "</td><td>" +
+        x[i].getElementsByTagName("pubDate")[0].childNodes[0].nodeValue +
+        "</td>"+
+        "</td></tr>";
+      }
+      document.getElementById("demo").innerHTML = table;
+    }
+</script>
+<!--
     <script>
     function loadDoc() {
       var xhttp = new XMLHttpRequest();
@@ -46,7 +80,7 @@
       var x = xmlDoc.getElementsByTagName("item");
       for (i = 0; i <x.length; i++) {
         table += "<tr><td>" +
-        "<img class=\"xmImg\" src=" + x[i].getElementsByTagName("enclosure")[0].getAttribute("url") + ">" +
+         "<img class=\"xmImg\" src=" + x[i].getElementsByTagName("enclosure")[0].childNodes[0].nodeValue + ">" +
          "</td><td>" +
         x[i].getElementsByTagName("category")[0].childNodes[0].nodeValue +
         "</td><td>" +
@@ -60,7 +94,7 @@
       }
       document.getElementById("demo").innerHTML = table;
     }
-    </script>
+  </script> -->
   </div>
 
 
@@ -69,6 +103,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="js/main.js" type="text/javascript"></script>
   </body>
 </html>
