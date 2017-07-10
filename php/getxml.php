65e3196038018c16b1cc0515b00a183d0d144224
Антон <>
2017-07-10 21:01:16 +0200

    new xml uploaded

--- php/getxml.php
+++ php/getxml.php
@@ -1,13 +1,20 @@
 <?php
-set_time_limit(0);
-$link="https://www.kommersant.ru/RSS/main.xml";
-$name="news.xml";
-if(@$data = file_get_contents($link)){
-echo "";
-}else{
-echo "no data could be loaded";
+
+$newsname = './news.xml';
+
+if (file_exists($newsname)) {
+    echo "";
+} else {
+  set_time_limit(0);
+  $link="https://www.kommersant.ru/RSS/corp.xml";
+  $name="news.xml";
+  if(@$data = file_get_contents($link)){
+  echo "";
+  }else{
+  echo "no data could be loaded";
+  }
+  $file = fopen($name,'w');
+  fwrite($file,$data); //и сохраним его
+  fclose($file);
 }
-$file = fopen($name,'w');
-fwrite($file,$data); //и сохраним его
-fclose($file);
 ?>
